0253253c2c832d938a89feef0756b7483a363b80
Yu Zhang <yu.zhang@ibm.com>
2019-02-17 02:25:36 -0500

    commit

--- load_test.py
+++ load_test.py
@@ -1,23 +1,8 @@
-# import uuid
-# from locust import HttpLocust, TaskSet, task
-#
-#
-# class MyTaskSet(TaskSet):
-#     @task
-#     def random_post_id(self):
-#         ess already in use: ('', 8089)
-#         self.client.post("/items/{}".format(id))
-#
-# class MyLocust(HttpLocust):
-#     task_set = MyTaskSet
-#     min_wait = 100
-#     max_wait = 100
+import uuid
+import requests
+import time
 
-import uuid,requests, time
 for i in range(220):
-    id = uuid.uuid4()
-    requests.post("http://localhost:5000/items/{}".format(id))
+    id_ = uuid.uuid4()
+    requests.post("http://localhost:5000/items/{}".format(id_))
     time.sleep(0.03)
-
-
-
